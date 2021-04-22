bc6337789a33351402c810d88cf3a5c02bf12935
Siwen Wang <wendydoog@gmail.com>
2021-04-22 00:47:54 -0500

    modified

---
+++ mongo-demo/Exercise/exercise/solution2.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises');
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String, 
+  tags: [ String ],
+  date: Date, 
+  isPublished: Boolean,
+  price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function getCourses() {
+  return await Course
+  //.find({ isPublished: true, tags: {$in: ['frontend','backend']}} )
+  .find({isPublished: true})
+  .or([{tags: 'frontend'},{tags:'backend'}])
+  .sort('-price')
+  .select('name author');
+  
+}
+
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+
+run();
+
+
